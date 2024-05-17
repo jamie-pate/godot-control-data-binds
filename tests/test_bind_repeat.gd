ce141282e21a5af6ec20b855e2fb313be50138e0
Herbert Ma <hma@fortinet.com>
2024-05-17 11:30:17 -0700

    Add support for callables

--- tests/test_bind_repeat.gd
+++ tests/test_bind_repeat.gd
@@ -2,6 +2,7 @@ extends GutTest
 
 const REPEATED_CONTROL_HOST = preload("./RepeatedControlHost.tscn")
 
+
 func test_bind_repeat():
 	var repeated_control_host = REPEATED_CONTROL_HOST.instantiate()
 	add_child_autoqfree(repeated_control_host)
@@ -9,7 +10,7 @@ func test_bind_repeat():
 	# not sure why this takes 2 frames to call the deferred method
 	await RenderingServer.frame_post_draw
 	await RenderingServer.frame_post_draw
-	
+
 	assert_eq(repeated_control_host.get_child_count(), 0)
 	repeated_control_host.model.append("text")
 	DataBindings.detect_changes()
